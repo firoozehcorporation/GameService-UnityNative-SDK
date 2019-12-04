ec61bd6e9914a306876bc5d9ef1fe6a491aaff0d
Alireza Ghodrati <apps.ghodrati1377@gmail.com>
2019-12-04 13:57:55 +0330

    Initial Commit

---
+++ Assets/Plugins/GameService/Utils/GameServiceInitializer.cs
@@ -0,0 +1,32 @@
+﻿using FiroozehGameService.Builder;
+using UnityEngine;
+using SystemInfo = FiroozehGameService.Models.Internal.SystemInfo;
+
+namespace GameService.Utils
+{
+    public class GameServiceInitializer : MonoBehaviour
+    {
+        public string ClientId;
+        public string ClientSecret;
+        
+        void Start()
+        {        
+            var systemInfo = new SystemInfo
+            {
+                DeviceModel = UnityEngine.SystemInfo.deviceModel,
+                DeviceName = UnityEngine.SystemInfo.deviceName,
+                DeviceType = UnityEngine.SystemInfo.deviceType.ToString(),
+                OperatingSystem = UnityEngine.SystemInfo.operatingSystem,
+                ProcessorCount = UnityEngine.SystemInfo.processorCount,
+                ProcessorFrequency = UnityEngine.SystemInfo.processorFrequency,
+                ProcessorType = UnityEngine.SystemInfo.processorType,
+                GraphicsDeviceName = UnityEngine.SystemInfo.graphicsDeviceName,
+                GraphicsDeviceVendor = UnityEngine.SystemInfo.graphicsDeviceVendor,
+                GraphicsMemorySize = UnityEngine.SystemInfo.graphicsMemorySize
+            };
+            var config = new GameServiceClientConfiguration(ClientId,ClientSecret,systemInfo);
+            FiroozehGameService.Core.GameService.ConfigurationInstance(config);
+        }
+       
+    }
+}
