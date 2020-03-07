4b41821630c5e723127d4de9bf1f3033f95fe6e5
Alireza Ghodrati <apps.ghodrati1377@gmail.com>
2020-03-07 17:03:27 +0330

    Remove Extra Functions

--- Assets/Plugins/GameService/Extra/AndroidSupport/Core/DelegateCore.cs
+++
@@ -1,8 +0,0 @@
-namespace Plugins.GameService.Utils.AndroidSupport.Core
-{
-    internal class DelegateCore
-    {
-        internal delegate void OnCallback(string result);
-        internal delegate void OnError(string error);
-    }
-}
